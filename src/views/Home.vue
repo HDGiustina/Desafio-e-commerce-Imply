bcfa14ffa920c8bd626e26e15452cb4b637b489a
Helena Della Giustina <helenadgiustina@gmail.com>
2022-11-04 16:22:18 -0300

    Complemento compra

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,33 @@
 <template>
     <main class="uk-height-viewport">
-        <section class="uk-section uk-background-cover uk-flex uk-flex-center" data-src="/img/fundo-home.jpg" uk-img>
+        <section class="uk-section uk-background-cover uk-flex uk-flex-center  " data-src="/img/fundo-home.jpg" uk-img>
             
-                <div class="uk-width-1-2 uk-margin-xlarge-left uk-margin-xlarge-right uk-light uk-text-center ">
-                    <h1>Os Melhores Tours!</h1>
-                    <p class="uk-text-bold">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo eaque enim perferendis recusandae architecto tempora nam! Voluptatibus a doloribus deleniti nesciunt alias sint ipsam beatae, possimus minima laboriosam id.</p>
+                <div class="uk-width-1-2 uk-margin-xlarge-left uk-margin-xlarge-right uk-text-center">
+                    <h1 class="uk-text-secondary ">Os Melhores Tours!</h1>
+                    <p class="uk-text-bold uk-text-secondary ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor nemo eaque enim perferendis recusandae architecto tempora nam! Voluptatibus a doloribus deleniti nesciunt alias sint ipsam beatae, possimus minima laboriosam id.</p>
                     
                 </div>
                 <div class="uk-border-rounded uk-card uk-card-default uk-card-hover uk-card-body uk-width-1-2 uk-margin-xlarge-right">
-                    <h3>Compre o seu ingreso</h3>
-                    <label for="data" class="uk-form-label">Selecione a data</label>
-                    <input type="date" name="data" id="data" class="uk-border-rounded uk-input">
-                    <label for="quantidade" class="uk-form-label">Informe quantos ingressos</label>
-                    <input type="number" name="quantidade" id="quantidade" min="0" class="uk-border-rounded uk-input">
-                    <label for="ingressos" class="uk-form-label">Selecione o produto</label>
-                    <select name="ingressos" id="ingressos" class="uk-border-rounded uk-select">
-
-                        <option default disabled>Selecione...</option>
-                        <option>*Back</option>
-
-                    </select>
+                    <form @submit.prevent="enviarDados" >
+                        <h3 class="uk-text-secondary">Compre o Seu Ingresso!</h3>
+                        <label for="data" class="uk-form-label">Selecione a data</label>
+                        <input type="date" name="data" id="data" class="uk-border-rounded uk-input">
+                        <label for="quantidade" class="uk-form-label">Informe quantos ingressos</label>
+                        <input type="number" name="quantidade" id="quantidade" min="0" class="uk-border-rounded uk-input">
+                        <label for="ingressos" class="uk-form-label">Selecione o Tour</label>
+                        <select name="ingressos" id="ingressos" class="uk-border-rounded uk-select" >
+    
+                            <option selected disabled>Selecione...</option>
+                            <option v-for="(tour, index) in Tours" :key="index">{{ tour }}</option>
+    
+                        </select>
+                        <label for="tipo" class="uk-form-label">Selecione o tipo de ingresso</label>
+                        <select name="tipo" id="tipo" class="uk-border-rounded uk-select">
+                            <option selected disabled>Selecione...</option>
+                            <option v-for="(tipo, index) in ingresso" :key="index">{{ tipo }}</option>
+                        </select>
+                        <button type="submit" class="uk-button uk-margin uk-align-center uk-background-secondary uk-text-muted uk-box-shadow-hover-large uk-border-rounded">Adicionar</button>
+                    </form>
                 </div>
         </section>
         <section class="uk-flex uk-margin-large-top uk-align-center uk-flex-center " >
@@ -27,7 +35,7 @@
             <div class="uk-position-relative uk-visible-toggle uk-light uk-width-xlarge " tabindex="-1" uk-slideshow>
                 
                 <ul class="uk-slideshow-items uk-border-rounded">
-                    <li v-for="(img, index) in imagens" v-bind:key="index">
+                    <li v-for="(img, index) in imagens" :key="index">
                         <img :src="img" alt="teste" uk-cover>
                     </li>
                 </ul>
@@ -55,7 +63,21 @@ import { createDOMCompilerError } from "@vue/compiler-dom";
                     '/img/carrossel2.jpg',
                     '/img/carrossel3.jpg',
                     '/img/carrossel4.jpg'
-                ]
+                ],
+                Tours: [
+                    'Tour Histórico',
+                    'Tour Educacional',
+                    'Tour Amanhecer'
+                ],
+                ingresso: ['Inteiro','Meia-entreda','PCDs']
+            }
+        },
+        methods: {
+            enviarDados(e){
+                let userInfo = {
+                   "data" = e.target.elements.email.value
+                }
+                console.log(userInfo)
             }
         }
     }
