df0ab1add1bb618836081fcb682918f6433300a7
HelenaDGiustina <helenadgiustina@gmail.com>
2022-11-05 23:55:58 -0300

    Estrutura Carrinho

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -1,11 +1,39 @@
 <template>
-    <div class="about">
-        <h1>Está é a página de Carrinho/Checkout</h1>
+    <div class="uk-container uk-margin-xlarge uk-column-1-2@m">
+        <section>
+            <h1 class="uk-text-center">Produtos no Carrinho</h1>
+            <ul class="uk-list uk-list-striped uk-column uk-overflow-auto" >
+                <li v-for="pedidos in carrinho" :key="pedidos.id">
+                    <p>{{ pedidos.tour }} - TIpo: {{ pedidos.type }}</p>
+                    <p>R$ {{ pedidos.pryce }}, 00</p>
+                </li>
+            </ul>
+        </section>
+        <section>
+            <h1 class="uk-text-center">Resumo do Pedido</h1>
+            <div class="uk-card uk-card-default uk-card-body uk-width-1-2@m uk-margin-xlarge-left">
+                <p>Itens: </p>
+                <p>Total:</p>
+                <hr>
+                <button class="uk-button uk-margin uk-align-center uk-background-primary uk-text-secondary uk-box-shadow-hover-large uk-border-rounded">Finalizar Compra</button>
+            </div>
+        </section>
     </div>
 </template>
 
 <script>
     export default {
-        name: 'Checkout'
+        name: 'Checkout',
+        data(){
+            return{
+                carrinho:[
+                {id: 1, tour: 'Tour Historico', type: 1, pryce: 18},
+                {id: 2, tour: 'Tour Historico', type: 2, pryce: 9},
+                {id: 3, tour: 'Tour Historico', type: 3, pryce: 9},
+                {id: 4, tour: 'Tour Educacional', type: 1, pryce: 12},
+                {id: 5, tour: 'Tour Educacional', type: 2, pryce: 6}
+                ]
+            }
+        }
     }
 </script>
--- src/views/Institucional.vue
+++ src/views/Institucional.vue
@@ -1,21 +1,20 @@
 <template>
     <main>
-        <div class="uk-margin-xlarge-left uk-margin-xlarge-right">
-        <h1>Sobre nós</h1>
-   
-    <div class="uk-text-justify">
-        <p>
-                Lorem ipsum dolor sit amet. Est rerum necessitatibus qui galisum quasi et corrupti beatae non deserunt 
-            quibusdam. Ut molestiae adipisci cum delectus modi eos nihil dolores est nostrum voluptate est aliquam 
-            galisum et ratione delectus non consequatur modi.Qui corrupti molestiae et quam voluptatibus est eveni
-            et dolor ut temporibus error id quod aspernatur et libero iusto. Ut amet architecto qui nihil voluptates 
-            est iste consequatur id vero autem ad voluptas internos qui numquam rerum ex earum similique. Sed modi 
-            nihil cum consectetur officia ut nostrum voluptas ea harum ipsum qui earum ducimus.Et eligendi iusto qui
-            iusto laboriosam ut velit asperiores ad reiciendis illum qui exercitationem voluptas sit quibusdam odio.
-            Eos culpa labore id magni ratione aut sint consectetur ex laborum reprehenderit!
-        </p>
-        </div>
-    </div>
+        <article class="uk-margin-xlarge-left uk-margin-xlarge-right uk-text-center uk-padding-large">
+            <h1>Sobre nós</h1>
+            <div class="uk-text-justify ">
+                <p>
+                        Lorem ipsum dolor sit amet. Est rerum necessitatibus qui galisum quasi et corrupti beatae non deserunt 
+                    quibusdam. Ut molestiae adipisci cum delectus modi eos nihil dolores est nostrum voluptate est aliquam 
+                    galisum et ratione delectus non consequatur modi.Qui corrupti molestiae et quam voluptatibus est eveni
+                    et dolor ut temporibus error id quod aspernatur et libero iusto. Ut amet architecto qui nihil voluptates 
+                    est iste consequatur id vero autem ad voluptas internos qui numquam rerum ex earum similique. Sed modi 
+                    nihil cum consectetur officia ut nostrum voluptas ea harum ipsum qui earum ducimus.Et eligendi iusto qui
+                    iusto laboriosam ut velit asperiores ad reiciendis illum qui exercitationem voluptas sit quibusdam odio.
+                    Eos culpa labore id magni ratione aut sint consectetur ex laborum reprehenderit!
+                </p>
+            </div>
+        </article>
 </main>
 </template>
 
