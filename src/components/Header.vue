324ad5ab860804640d8217a203649d92bf3c901d
johanes-rusch <johanesrusch@gmail.com>
2022-11-05 21:10:20 -0300

    Adicionando os ícones

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,10 @@
                     <div class="uk-width-1-4 uk-align-left uk-margin-xlarge-left"><img src="/img/parque_das_palmeiras-header.png" alt="Logo do site"></div>
                 <div class="uk-navbar-right">
                 <ul class="uk-navbar-nav">
-                    <li class="uk-parent"><a class="uk-text-large uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text" href="/"><span uk-icon="home"></span> Home</a></li>
+                    <li class="uk-parent"><a class="uk-text-large uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text" href="/">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12 5.69L17 10.19V18H15V12H9V18H7V10.19L12 5.69M12 3L2 12H5V20H11V14H13V20H19V12H22" />
+                        </svg> Home</a></li>
                     <li class="uk-parent uk-button-text">
                         <button class="uk-text-uppercase uk-text-default uk-margin-large-top uk-text-secondary uk-button-default" uk-toggle="target: #my-id">
                             <svg style="width:24px;height:24px" viewBox="0 0 24 24">
@@ -84,7 +87,11 @@
                         </div>
                     </li>
                     <li>
-                        <a class="uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text" href="/"><span uk-icon="user"></span>User</a>
+                        <a class="uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text" href="/">
+                            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M12,4A4,4 0 0,1 16,8A4,4 0 0,1 12,12A4,4 0 0,1 8,8A4,4 0 0,1 12,4M12,6A2,2 0 0,0 10,8A2,2 0 0,0 12,10A2,2 0 0,0 14,8A2,2 0 0,0 12,6M12,13C14.67,13 20,14.33 20,17V20H4V17C4,14.33 9.33,13 12,13M12,14.9C9.03,14.9 5.9,16.36 5.9,17V18.1H18.1V17C18.1,16.36 14.97,14.9 12,14.9Z" />
+                            </svg>
+                         User</a>
                         <div uk-dropdown="mode:click" class="uk-list">
                             <li><span uk-icon="user"></span><RouterLink to="/perfil" class="uk-text-secondary uk-text-decoration-none uk-button-text">Perfil</RouterLink></li>
                             <li><RouterLink to="/pedidos" class="uk-text-secondary uk-text-decoration-none uk-button-text">Pedidos</RouterLink></li>
@@ -92,7 +99,11 @@
                             <li><span uk-icon="sign-out"></span><RouterLink to="/" class="uk-text-secondary uk-text-decoration-none uk-button-text">Sair</RouterLink></li>
                         </div>
                     </li>
-                    <li class="uk-parent"><a class="uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text uk-margin-xlarge-right" href="/checkout"><span uk-icon="cart"></span> Carrinho</a></li>
+                    <li class="uk-parent"><a class="uk-margin-medium-top uk-text-secondary uk-text-decoration-none uk-button-text uk-margin-xlarge-right" href="/checkout">
+                        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M17,18A2,2 0 0,1 19,20A2,2 0 0,1 17,22C15.89,22 15,21.1 15,20C15,18.89 15.89,18 17,18M1,2H4.27L5.21,4H20A1,1 0 0,1 21,5C21,5.17 20.95,5.34 20.88,5.5L17.3,11.97C16.96,12.58 16.3,13 15.55,13H8.1L7.2,14.63L7.17,14.75A0.25,0.25 0 0,0 7.42,15H19V17H7C5.89,17 5,16.1 5,15C5,14.65 5.09,14.32 5.24,14.04L6.6,11.59L3,4H1V2M7,18A2,2 0 0,1 9,20A2,2 0 0,1 7,22C5.89,22 5,21.1 5,20C5,18.89 5.89,18 7,18M16,11L18.78,6H6.14L8.5,11H16Z" />
+                        </svg>
+                     Carrinho</a></li>
                 </ul>
                 </div>
             </nav>
